989fc37061e1072c744d0402eb5d00ca33a4cd1a
EmersonGald1n0 <emersonprof@outlook.com>
2023-03-09 19:26:01 -0300

    Add class midia, atributos, construtor + get/set

---
+++ src/Midia.java
@@ -0,0 +1,32 @@
+public class Midia {
+    private String title;
+    private int year;
+    private String gender;
+
+    Midia () {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
