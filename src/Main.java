30f55c57bb7cd4d0b31f34116d52cd8d289f502e
EmersonGald1n0 <emersonprof@outlook.com>
2023-03-13 19:10:00 -0300

    Att método main para iniciar o projeto

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         
-        Playlist list = new Playlist();
+        Playlist list = new Playlist(); // Instanciando a classe Playlist
 
-        BibliotecaDeMidias biblioteca = new BibliotecaDeMidias("Emerson", list, list);
+        BibliotecaDeMidias biblioteca = new BibliotecaDeMidias("Emerson", list, list); // Instanciando a classe BibliotecaDeMidias e passando os parâmetros para o construtor
 
-        biblioteca.play_playlist();
+        biblioteca.play_playlist(); // Chamando o método play_playlist() da classe BibliotecaDeMidias
     }
 }
