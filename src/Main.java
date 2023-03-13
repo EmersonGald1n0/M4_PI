3e186ee1ec7bf09438bb255e6d0ac3bb13b95c05
EmersonGald1n0 <emersonprof@outlook.com>
2023-03-13 19:09:27 -0300

    att método main para executar o projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Biblioteca de Mídias");
+        
+        Playlist list = new Playlist();
+
+        BibliotecaDeMidias biblioteca = new BibliotecaDeMidias("Emerson", list, list);
+
+        biblioteca.play_playlist();
     }
 }
