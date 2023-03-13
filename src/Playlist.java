aad1e3b47c74413ccd137a3537531908574749be
EmersonGald1n0 <emersonprof@outlook.com>
2023-03-13 19:12:07 -0300

    Add métodos gerais para executar a playlist e outras funções

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Playlist {
     private String namePlaylist;
     private Midia[] executionOrder;
@@ -31,6 +33,54 @@ public class Playlist {
         this.currentMedia = currentMedia;
     }
 
+    // Método playlist com parâmetros para verificar se a mídia é filme ou música e criar a playlist
+    public void playlist(boolean verificaMidia, int i) {
+        create_playlist(verificaMidia, i);
+    }
+
+    // Método para criar a playlist
+    public void create_playlist(boolean verificaMidia, int i) {
+        Scanner input = new Scanner(System.in);
+        String question = "(1)Play \n(2)Next \n(3)Previous \n(4)Shuffle \n(5)Exit \n";
+        String response = null;
+        String statusActive = null;
+        
+        if (verificaMidia == true) {
+            if(i == 0) {
+                this.create_movie();
+            }
+            statusActive = "Assistindo filme: ";
+        } else {
+            if(i == 0) {
+                this.create_music();
+            }
+            statusActive = "Ouvindo música: ";
+        }
+
+        int count = 0;
+        while (count == 0) {
+            System.out.println(statusActive + this.executionOrder[this.currentMedia].getTitle());
+            System.out.println(question);
+            response = input.nextLine();
+            if (response.equals("1")) {
+                System.out.println("Tocando mídia...");
+            } else if (response.equals("2")) {
+                nextMedia();
+            } else if (response.equals("3")) {
+                previousMedia();
+            } else if (response.equals("4")) {
+                shuffleMedia();
+                System.out.println("Misturando mídias...");
+            } else if (response.equals("5")) {
+                System.out.println("Saindo da playlist...");
+                System.out.println("Obrigado por utilizar nosso serviço!");
+                count++;
+            } else {
+                System.out.println("Opção inválida!");
+            }
+        }
+    }
+
     // Método para tocar a próxima mídia da playlist
     public void nextMedia() {
         if (this.currentMedia < this.executionOrder.length - 1) {
@@ -46,6 +96,73 @@ public class Playlist {
             this.currentMedia--;
         } else {
             this.currentMedia = this.executionOrder.length - 1;
+        }
+    }
+
+    // Método para misturar a ordem de execução das mídias da playlist
+    public void shuffleMedia() {
+        for (int i = 0; i < this.executionOrder.length; i++) {
+            int random = (int) (Math.random() * this.executionOrder.length);
+            Midia temp = this.executionOrder[i];
+            this.executionOrder[i] = this.executionOrder[random];
+            this.executionOrder[random] = temp;
+        }
+    }
+
+    // Método para criar midia filme
+    public void create_movie(){
+        Ator[] cast = new Ator[7];
+        cast[0] = new Ator("Ator 1", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+        cast[1] = new Ator("Ator 2", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+        cast[2] = new Ator("Ator 3", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+        cast[3] = new Ator("Ator 4", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+        cast[4] = new Ator("Ator 5", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+        cast[5] = new Ator("Ator 6", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+        cast[6] = new Ator("Ator 7", "18/08/1974", "Oscar 2022", "Mascuilino", "Formação 1");
+
+        Filme[] movies = new Filme[7];
+        movies[0] = new Filme("Filme 1", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 1", "Produtor 1");
+        movies[1] = new Filme("Filme 2", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 2", "Produtor 2");
+        movies[2] = new Filme("Filme 3", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 3", "Produtor 3");
+        movies[3] = new Filme("Filme 4", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 4", "Produtor 4");
+        movies[4] = new Filme("Filme 5", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 5", "Produtor 5");
+        movies[5] = new Filme("Filme 6", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 6", "Produtor 6");
+        movies[6] = new Filme("Filme 7", 2021, "Ação", 1.30, "Longa metragem", cast, "Diretor 7", "Produtor 7");
+
+        this.executionOrder = movies;
+    }
+
+    // Método para criar músicas
+    public void create_music(){
+        Artista[] artist = new Artista[7];
+        artist[0] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+        artist[1] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+        artist[2] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+        artist[3] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+        artist[4] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+        artist[5] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+        artist[6] = new Artista("Chorão", "12/10/1971", "MTV Awards", "Masculino");
+
+        Musica[] music = new Musica[7];
+        music[0] = new Musica("Musica 1", 2021, "Rock", 1.30, artist[0], 5);
+        music[1] = new Musica("Musica 2", 2021, "Rock", 1.30, artist[1], 5);
+        music[2] = new Musica("Musica 3", 2021, "Rock", 1.30, artist[2], 5);
+        music[3] = new Musica("Musica 4", 2021, "Rock", 1.30, artist[3], 5);
+        music[4] = new Musica("Musica 5", 2021, "Rock", 1.30, artist[4], 5);
+        music[5] = new Musica("Musica 6", 2021, "Rock", 1.30, artist[5], 5);
+        music[6] = new Musica("Musica 7", 2021, "Rock", 1.30, artist[6], 5);
+
+        this.executionOrder = music;
+    }
+
+    // Método para mostrar status da playlist
+    public void show_status(Midia[] list, int i, boolean verificaMidia) {
+        if(verificaMidia){
+            Filme movie = (Filme) list[i];
+            movie.getStatusFilm();
+        } else {
+            Musica music = (Musica) list[i];
+            music.getStatusMusic();
         }
     }
 }
